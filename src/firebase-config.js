74a3308f0b8cc379839a023a97181ec2994aad13
nikosnoir <2023680176@student.uitm.edu.my>
2025-01-02 21:55:37 +0800

    signing

---
+++ src/firebase-config.js
@@ -0,0 +1,12 @@
+export const environment = {
+    firebase: {
+      apiKey: "AIzaSyDBxk5oZyt0KMSS9VV67Oq3ALTOdL2EmcA",
+      authDomain: "friendlychat-42cb5.firebaseapp.com",
+      databaseURL: 'https://PROJECT_ID.firebaseio.com',
+      projectId: "friendlychat-42cb5",
+      storageBucket: "friendlychat-42cb5.firebasestorage.app",
+      messagingSenderId: "1096127266017",
+      appId: "1:1096127266017:web:61f326306f9c493d2c0ba0",
+      measurementId: 'G-MEASUREMENT_ID',
+    },
+  };
