af4c39a84b1d83fcea95d44ec11024b09209b3e6
nikosnoir <2023680176@student.uitm.edu.my>
2025-01-02 21:55:56 +0800

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
+    apiKey: "AIzaSyDBxk5oZyt0KMSS9VV67Oq3ALTOdL2EmcA",
+    authDomain: "friendlychat-42cb5.firebaseapp.com",
     databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
+    projectId: "friendlychat-42cb5",
+    storageBucket: "friendlychat-42cb5.firebasestorage.app",
+    messagingSenderId: "1096127266017",
+    appId: "1:1096127266017:web:61f326306f9c493d2c0ba0",
     measurementId: 'G-MEASUREMENT_ID',
   },
 };
