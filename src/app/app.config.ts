6371d1cd22337c04855c59bf1f34043403669092
nikosnoir <2023680176@student.uitm.edu.my>
2025-01-02 21:07:44 +0800

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -19,6 +19,6 @@ export const appConfig: ApplicationConfig = {
       provideStorage(() => getStorage()),
       provideMessaging(() => getMessaging())
     ),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-42cb5","appId":"1:1096127266017:web:61f326306f9c493d2c0ba0","storageBucket":"friendlychat-42cb5.firebasestorage.app","apiKey":"AIzaSyDBxk5oZyt0KMSS9VV67Oq3ALTOdL2EmcA","authDomain":"friendlychat-42cb5.firebaseapp.com","messagingSenderId":"1096127266017"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
